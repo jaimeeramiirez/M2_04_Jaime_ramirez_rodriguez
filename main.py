5cb6b7866d14b0aa1e5f072b4261af6f775c564c
jaimeeramiirez <113980639+jaimeeramiirez@users.noreply.github.com>
2022-10-05 10:20:48 +0000

    EJERCICIO 1, APARTADOS 1,2,3 Y 4

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-#Crearemos una lista con el nombre "Lista", y una tupla con el nombre "Tupla", para hacer referencia al segundo valor de la lista tendremos que indicarlo. Ya que las listas y las tuplas relacionan el primer elemento con el número 0, el segundo elemento estara vincluado con el número 1 y de la misma manera el tercer elemento de la tupla con el 2. Las tuplas no pueden modificarse por lo que al principio del código deberemos nombrar los elemento de la mimsa.
-
-
+#EJERCICIO1.- Crearemos una lista con el nombre "Lista", y una tupla con el nombre "Tupla", para hacer referencia al segundo valor de la lista tendremos que indicarlo. Ya que las listas y las tuplas relacionan el primer elemento con el número 0, el segundo elemento estara vincluado con el número 1 y de la misma manera el tercer elemento de la tupla con el 2. Las tuplas no pueden modificarse por lo que al principio del código deberemos nombrar los elemento de la mimsa. Para modificar una lista podremos añadir o eliminar elementos; en nuestro caso, con la función ".remove" hemos eliminado el segundo elemento de la lista, lo cual no afecta al principio del código ya que el cambio es posterior
 
 print("\n")
 
